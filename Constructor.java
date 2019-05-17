7ee411d2939ac223d81113949fdcebdd81ae1057
Gbemisola N <gbemikp@.com>
2019-05-17 12:16:47 +0100

    Constructor and Operation assignment

---
+++ Constructor.java
@@ -0,0 +1,15 @@
+// program to create a constructor.
+//Author: Gbemisola
+public class Constructor{
+    String name;
+    int age;
+
+    public Constructor (String name, int age){
+        this.name = name;
+        
+        
+    }
+
+    
+    
+}
